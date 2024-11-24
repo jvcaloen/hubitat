aae6b6984800ed390d067b7d750e7a6638bfcd89
jvcaloen <jean.vancaloen@gmail.com>
2024-11-24 07:54:36 +0100

    Update leaf2mqtt.groovy
    added reconnect and keepalive to avoid mqtt connection dropping

--- Drivers/leaf2mqtt.groovy
+++ Drivers/leaf2mqtt.groovy
@@ -11,14 +11,20 @@
 // Welcome to copy and use at your own risk. 
 //
 // 2024-07-03 - v1 - Jean van Caloen simple battery percentage & last update time in state
+// 2024-11-24 - v2 - Jean van Caloen added reconnect and keepalive to avoid mqtt connection dropping
 //
 
 metadata {
    definition (name: "Nissan Leaf Battery", namespace: "jvcaloen", author: "Jean") {
-      capability "Battery"
-      capability "PresenceSensor" // present means connected to mqtt
-      capability "Initialize" // button to (re)connect to mqtt
-      capability "Refresh"   // provide button to disconnect from mqtt - client status doesn't seem to trigger here :( 
+       capability "Battery"
+       capability "Outlet"
+       capability "PresenceSensor" // present means connected to mqtt
+       capability "Initialize" // button to (re)connect to mqtt
+       capability "Refresh"   // provide button to disconnect from mqtt - client status doesn't seem to trigger here :( 
+       
+       attribute "charging", "string"
+       
+       singleThreaded: true // preventing more than one overlapping wake of the driver code for the same device
    }
 
    preferences {
@@ -34,6 +40,13 @@ def initialize() {
     interfaces.mqtt.connect(mqttUrl, "hubitat-leaf-driver", mqttUsername, mqttPassword)
     interfaces.mqtt.subscribe("leaf/battery/percentage")
     interfaces.mqtt.subscribe("leaf/battery/lastUpdatedDateTimeUtc")
+    interfaces.mqtt.subscribe("leaf/battery/charging")
+    keepalive()
+    
+    if (!interfaces.mqtt.isConnected()) { 
+        runIn(60, "initialize") 
+        log.debug "not connected, initialize runIn 60s planned"
+    }
     log.debug "initialized()"
 }
 
@@ -41,13 +54,17 @@ def parse(String message) {
     // log.debug "parse() received: ${message}"
     mapped = interfaces.mqtt.parseMessage(message)
     log.debug "parse() mapped: ${mapped}"
-    if (mapped.topic == "leaf/battery/percentage" && mapped.payload != state.battery) { 
-        sendEvent(name:"battery", value: mapped.payload, unit: "%", isStateChange: true)
-        state.battery = mapped.payload
+    if (mapped.topic == "leaf/battery/percentage") { 
+        sendEvent(name:"battery", value: mapped.payload, unit: "%")
     }
     if (mapped.topic == "leaf/battery/lastUpdatedDateTimeUtc") {
         state.lastUpdatedDateTimeUtc = toDateTime(mapped.payload)
     }
+    if (mapped.topic == "leaf/battery/charging") {
+        sendEvent(name:"charging", value: mapped.payload)
+        sendEvent(name:"switch", value: mapped.payload)
+    }
+    keepalive()
 }
 
 def installed() {
@@ -65,12 +82,26 @@ def mqttClientStatus(String message) {
 
     if (message == "Status: Connection succeeded") { 
         log.debug "mqttClientStatus(): change to present"
-        sendEvent(name:"presence", value: "present", isStateChange: true)
+        sendEvent(name:"presence", value: "present", description: message, isStateChange: true)
     }
-    else if (message != "Status: Connection succeeded") { 
+    
+    else if (message == "Status: Disconnected via disconnect()"){
         log.debug "mqttClientStatus(): change to not present"
-        sendEvent(name:"presence", value: "not present", isStateChange: true)
+        sendEvent(name:"presence", value: "not present", description: message, isStateChange: true)
     }
+    else 
+    {
+        
+        if(!(interfaces.mqtt.isConnected())) {
+            log.debug "mqttClientStatus(): change to not present"
+            sendEvent(name:"presence", value: "not present", description: message, isStateChange: true)
+         
+            // pause + reconnect 
+            runIn(60, "initialize")
+            log.debug "initialize planned in 60s"
+        }
+    }
+    
 }
 
 def refresh() {
@@ -84,3 +115,10 @@ def disconnect() {
     mqttClientStatus("Status: Disconnected via disconnect()");
 }
 
+void keepalive() {
+    if (!interfaces.mqtt.isConnected()) { initialize() }
+    if (interfaces.mqtt.isConnected()) {
+        interfaces.mqtt.publish("hubitat-leaf-driver/keepalive", (String)now())
+        runIn(300, "keepalive")
+    }
+}
